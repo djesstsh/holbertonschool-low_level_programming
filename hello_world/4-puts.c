57f6839461466c4a60fd577fa71acafbc28508a9
djesstsh <dtshobo15@gmail.com>
2024-10-21 13:18:57 +0000

    kjh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
  #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle"
+ * main - Prints "Programming is like building a multilingual puzzle
  *
  * Return: Always 0.
  */
