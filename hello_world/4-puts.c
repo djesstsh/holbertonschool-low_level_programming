c21635f60a5149225a1970f1d4575e5399a76301
djesstsh <dtshobo15@gmail.com>
2024-10-21 13:49:23 +0000

    kjh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,9 @@
  #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Description: Calls puts and receives value returned by puts after its execution.
+ *Description: Calls puts and receives value returned by puts after its execution
  * Return: Always return 0 to indicate success
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
