710b5680e5b0d41bb2725e0fe5ed71fdf1e42814
djesstsh <dtshobo15@gmail.com>
2024-10-22 08:37:22 +0000

    jh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /*
  * main - Entry point
+ *
  * Description : Calls puts and receives value returned
  * Return : Always return 0 to indicate success
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
