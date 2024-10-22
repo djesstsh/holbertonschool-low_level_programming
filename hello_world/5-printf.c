1dd7c2b520262d1bb17b60a04b5f7c1f3f97dffc
djesstsh <dtshobo15@gmail.com>
2024-10-22 08:52:14 +0000

    kjhg

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:Calls printf and receives value returned
+ *
+ * Return:Always return 0 to indicate success
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+}
