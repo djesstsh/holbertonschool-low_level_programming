565d53972d010a4c9c191c272097cbc81bdf5358
djesstsh <dtshobo15@gmail.com>
2024-10-23 10:03:25 +0000

    jh

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,13 @@
 
 int main(void)
 {
-	int num;
+	{
+		int num;
 
-	for (num = '0'; num < 10; num++)
+		for (num = '0'; num < 10; num++)
 
-	printf("%d", num);
+		printf("%d", num);
+	}
 
 	putchar('\n');
 	return (0);
