2932ef04e5d06cceed821978c5acbf3286dad8b8
djesstsh <dtshobo15@gmail.com>
2024-10-23 12:14:40 +0000

    kj

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int main(void)
+{
+	char tab;
+
+	for (tab = 'z' ; tab >= 'a' ; tab--)
+	{
+		putchar(tab);
+	}
+
+	putchar(tab);
+	return (0);
+}
