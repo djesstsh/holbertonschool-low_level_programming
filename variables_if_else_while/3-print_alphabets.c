50048acc44d0a0c3eaa9eb39c61528d48dbe1d83
djesstsh <dtshobo15@gmail.com>
2024-10-23 08:34:35 +0000

    ijh

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	return (0);
+}
