d24b63f24dbe88ad8048995b24002792c68b4856
djesstsh <dtshobo15@gmail.com>
2024-10-27 15:54:28 +0000

    iuh

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - Computes absolue value
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int _abs(int)
+{
+	int avi;
+
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int print_last_digit(int p)
+{
+	return (p % 10);
+}
