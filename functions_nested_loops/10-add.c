1f844b83dd83ce9b0610a24ea7c4fb757f2c3707
djesstsh <dtshobo15@gmail.com>
2024-10-27 17:26:48 +0000

    kjh

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integer
+ *
+ * @a:parameter
+ *
+ * @b:parameter
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
