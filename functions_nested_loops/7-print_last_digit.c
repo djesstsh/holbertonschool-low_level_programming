7bd7c5bdb67b6a6e7376466a899a1ec278680865
djesstsh <dtshobo15@gmail.com>
2024-10-27 16:31:03 +0000

    kjh

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,20 @@
 /**
  * print_last_digit - Print the last digit
  *
+ * @p:parameter
+ *
  * Return:Always 0 to indicate success
  */
 
 int print_last_digit(int p)
 {
-	return (p % 10);
+	int l = p % 10;
+
+	if (l < 0)
+	{
+		l = -l;
+	}
+
+	_putchar(l + '0');
+	return (l);
 }
