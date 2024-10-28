6dd10c21b8c371d16e51afcac5199a26aaeb4202
djesstsh <dtshobo15@gmail.com>
2024-10-28 09:40:56 +0000

    kjh

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a:parameter
+ *
+ * @b:parameter
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
