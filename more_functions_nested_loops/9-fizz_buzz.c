2ea3a8a8f06386b2e1d7f1a4226734b6ebe957a6
djesstsh <dtshobo15@gmail.com>
2024-10-29 14:29:28 +0000

    jh

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 to indicate success
+ */
+
+int main(void)
+{
+	int pl;
+
+	for (pl = 1; pl <= 100; pl++)
+	{
+		if (pl % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (pl % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (pl % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", pl);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
