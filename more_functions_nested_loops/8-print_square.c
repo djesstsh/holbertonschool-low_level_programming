d7006fdbbbf3f309d820f8335f683d720916d072
djesstsh <dtshobo15@gmail.com>
2024-10-29 13:50:44 +0000

    jh

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size:parameter
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int pl, lig;
+
+		for (pl = 0; pl < size; pl++)
+		{
+			for (lig = 0; lig < size; lig++)
+			{
+				_putchar('#');
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	_putchar('\n');
+}
