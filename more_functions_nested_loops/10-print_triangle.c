37cdb8783ea25b475cff0fe0d2679c2a33114c39
djesstsh <dtshobo15@gmail.com>
2024-10-30 08:56:08 +0000

    kjh

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ */
+
+void print_triangle(int size)
+{
+	int pl, lig;
+	int bun;
+
+	if (size > 0)
+	{
+		for (pl = 0; pl < size; pl++)
+		{
+			bun = size - pl - 1;
+			for (lig = 0; lig < size; lig++)
+			{
+				if (bun > lig)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
