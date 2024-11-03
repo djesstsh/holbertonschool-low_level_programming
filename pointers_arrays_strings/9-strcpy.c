773c2f34f5ad8759b0b5544b67e293738ff55ecc
djesstsh <dtshobo15@gmail.com>
2024-11-03 18:24:28 +0000

    loj

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string
+ * @dest:parameter
+ * @src:parameter
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int pl, lig;
+
+	pl = 0;
+
+	while (src[pl] != '\0')
+	{
+		pl++;
+	}
+
+	for (lig = 0; lig < pl; lig++)
+	{
+		dest[lig] = src[lig];
+	}
+	dest[lig] = '\0';
+
+	return (dest);
+}
