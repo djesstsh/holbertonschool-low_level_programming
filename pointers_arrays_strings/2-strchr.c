4ecd0459ab9109fdf362ea2149ddf95e97813951
djesstsh <dtshobo15@gmail.com>
2024-11-04 15:03:50 +0000

    kjh

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s:parameter
+ * @c:parameter
+ * Return:Always 0 to indicate success
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (
