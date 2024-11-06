ef0a3e34d0907ff2c8cb5a3a1dd7992e16e8cad9
djesstsh <dtshobo15@gmail.com>
2024-11-06 13:31:05 +0000

    kjh

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strstr - locate a substring
+ * @haystack:parameter
+ * @needle:parameter
+ * Return:Always 0 to indicate success
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int pl, lig;
+
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+
+	for (pl = 0; haystack[pl] != '\0'; pl++)
+	{
+		for (lig = 0; needle[lig] != '\0'; lig++)
+		{
+			if (needle[lig] == '\0')
+			{
+				return (&haystack[pl]);
+			}
+		}
+	}
+	return (NULL);
+}
