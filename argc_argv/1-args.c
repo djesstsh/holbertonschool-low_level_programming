1c5aa3222f49462fbb0786eeea4e1d10488b1bfc
djesstsh <dtshobo15@gmail.com>
2024-11-08 21:50:51 +0000

    kjh

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc:parameter
+ * @argv:parameter
+ * Return:Always 0 to indicate success
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
