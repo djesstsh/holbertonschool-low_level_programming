a6763c59a817af5556bdbd3c3cca1f01e2815072
djesstsh <dtshobo15@gmail.com>
2024-11-08 14:54:11 +0000

    kjh

---
+++ functions_nested_loops/15-test.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = -i;
+	}
+	return (i);
+}
--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,4 +7,14 @@
 
 int factorial(int n)
 {
-	n = 
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
